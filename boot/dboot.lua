51150450ae775055133eef52e029832bf5a03b95
XDuskAshes <thegaymeradummy@gmail.com>
2023-08-03 18:46:38 -0400

    Changed booting to be cleaner
    startup.lua now handles basefs and basefiles checks entirely, performing
    these before dboot is touched.

--- boot/dboot.lua
+++ boot/dboot.lua
@@ -1,97 +1,28 @@
 --dawn default boot program
 --by dusk
 
-term.clear()
-term.setCursorPos(1,1)
-
-local handle = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dawn/idev/install/fs"))
-    local bfs = {}
-    repeat
-        local a = handle.readLine()
-        table.insert(bfs,a)
-    until a == nil
-handle.close()
-
-local handle2 = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dawn/idev/install/files"))
-    local bfiles = {}
-    repeat
-        local a = handle2.readLine()
-        table.insert(bfiles,a)
-    until a == nil
-handle2.close()
+local ke = require "/kernel"
 
 local fromROM = {
-    "[ /bin/cd.lua ]" == "/rom/programs/cd.lua",
-    "[ /bin/ls.lua ]" == "/rom/programs/list.lua",
-    "[ /bin/edit.lua ]" == "/rom/programs/edit.lua"
+    "/bin/cd.lua",
+    "/bin/ls.lua",
+    "/bin/edit.lua"
 }
 
-if fs.exists("/etc/config/simpleboot") then --simple looking boot anim ( [ ########## ] )
-    term.setCursorPos(11,8)
-    print("Dawn is booting, please wait.")
-    term.setCursorPos(18,10)
-    write("[ ---------- ]")
-    term.setCursorPos(20,10)
-    for i,v in pairs(bfs) do
-        if fs.exists(v) ~= true then
-            fs.makeDir(v)
+for k,v in pairs(fromROM) do
+    if fs.exists(v) then
+        ke.scrMSG(1,"exists: "..v)
+    else
+        if v == "/bin/cd.lua" then
+            fs.copy("/rom/programs/cd.lua",v)
+        elseif v == "/bin/ls.lua" then
+            fs.copy("/rom/programs/list.lua",v)
+        elseif v == "/bin/edit.lua" then
+            fs.copy("/rom/programs/edit.lua",v)
         end
-        sleep(0.01)
+        ke.scrMSG(3,"had to copy from rom: "..v)
     end
-
-    textutils.slowWrite("#####")
-
-    for i,v in pairs(bfiles) do
-        if fs.exists(v) ~= true then
-            handle = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dawn/idev/"..v))
-            local toWrite = {}
-            repeat
-                local a = handle.readLine()
-                table.insert(toWrite,a)
-            until a == nil
-            handle.close()
-            handle = fs.open(v,"w")
-            for _,v in pairs(toWrite) do
-                handle.writeLine(v)
-            end
-            handle.close()
-        end
-        sleep(0.01)
-    end
-
-    textutils.slowWrite("#####")
-
-    term.setCursorPos(6,8)
-    sleep(0.1)
-    print("Dawn has finished booting. Going to login.")
-    sleep(1)
-    term.clear()
-    term.setCursorPos(1,1)
-    shell.run("/bin/login.lua")
-else --logging and such
-    for i,v in pairs(bfs) do
-        if fs.exists(v) ~= true then
-            fs.makeDir(v)
-            printError(v.." doesn't exist. dir made.")
-        else
-            print(v.." exists")
-        end
-        sleep(0.01)
-    end
-
-    for i,v in pairs(bfiles) do
-        if fs.exists(v) ~= true then
-            printError(4,v.." doesn't exist. download.")
-            shell.run("wget","https://raw.githubusercontent.com/XDuskAshes/dawn/idev/"..v,v)
-        else
-            print(v.." exists")
-        end
-        sleep(0.01)
-    end
-
-    if fs.exists("/tmp/ccpcBug") then
-        fs.delete("/tmp/ccpcBug")
-        print("dbios can be entered as a username")
-    end
-    shell.run("/bin/login.lua")
 end
+
+ke.scrMSG(3,"'dbios' as username enters dbios.")
+shell.run("/bin/login.lua")
--- startup.lua
+++ startup.lua
@@ -1,26 +1,8 @@
 shell.run("set shell.allow_disk_startup false")
 term.clear()
-if fs.exists("/etc/logs/startup") then
-    fs.delete("/etc/logs/startup")
-end
 
-local per = peripheral.getNames()
-local yesSpeaker = false
-local s
-for i,v in ipairs(per) do
-    local t = peripheral.getType(v)
-        if t == "speaker" then
-            yesSpeaker = true
-            s = peripheral.find("speaker")
-            s.playNote("bit",3,18)
-            s.playNote("bit",3,23)
-            s.playNote("bit",3,18)
-            sleep(0.3)
-            s.playNote("bit",3,19)
-            s.playNote("bit",3,24)
-            s.playNote("bit",3,19)
-        end
-end
+local spinner = {"|","/","-","\\"}
+local stage = 0
 
 local handle = fs.open("/etc/logs/startup","w")
 handle.writeLine("DAWN CP-BSL (Computer Post-Boot State Log)")
@@ -57,95 +39,85 @@ print("|")
 print("==================")
 local c = os.clock()
 handle.writeLine("Basic info retrieved in: "..c.."s")
+handle.close()
+sleep(1)
+term.clear()
+term.setCursorPos(1,1)
 
-if tSizex == 132 and tSizey == 49 then
-    handle.writeLine("CCPC 'bigscreen' is on from boot")
-    handle.writeLine("Avoiding bug with keys, suggestion will be passed through with dboot.")   
-    print("Avoiding bug with keys, suggestion will be passed through with dboot.")
-    local c = os.clock()
-    handle.writeLine("Reached boot in: "..c.."s")
-    handle.close()
-    if fs.exists("/etc/ccpcBug") then
-        fs.delete("/etc/ccpcBug")
-    end
-    fs.copy("/etc/file","/etc/ccpcBug")
-    sleep(1)
-    shell.run("/boot/dboot.lua")
-else
-    if tSizex < 51 or tSizex > 51 or tSizey < 19 or tSizey > 19 then
-    handle.writeLine("tSizex or tSizey smaller than required (51)")
-    print("DAWN is made for standard computers.")
-    if not periphemu then
-        handle.writeLine("CCPC Not detected")
-        print("Please run this on a normal computer via file transfer in disk drive.")
-        handle.writeLine("Printed disk drive message")
-        sleep(1)
-        handle.writeLine("Reached shutdown")
-        handle.close()
-        os.shutdown()
+local handle = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dawn/idev/install/fs"))
+    local bfs = {}
+    repeat
+        local a = handle.readLine()
+        table.insert(bfs,a)
+    until a == nil
+handle.close()
+
+local handle2 = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dawn/idev/install/files"))
+    local bfiles = {}
+    repeat
+        local a = handle2.readLine()
+        table.insert(bfiles,a)
+    until a == nil
+handle2.close()
+
+local x,y = term.getCursorPos()
+for k,v in pairs(bfs) do
+    if fs.exists(v) then
+        term.setCursorPos(x,y)
+        stage = (stage % 4) + 1
+        write("running basefs check... ".. spinner[stage].." ")
     else
-        handle.writeLine("CCPC detected, hanging to resize")
-        print("Please resize the screen until you see the letter 'A' below.")
-        repeat
-            sleep(0.01)
-            tSizex, tSizey = term.getSize()
-            term.setCursorPos(1,1)
-            term.clear()
-            print("Please resize the screen until you see '51 19' below.")
-            print(tSizex,tSizey)
-        until tSizex == 51 and tSizey == 19
-        print("A")
-        handle.writeLine("resize success.")
+        fs.makeDir(v)
+        term.setCursorPos(x,y)
+        stage = (stage % 4) + 1
+        write("running basefs check... ".. spinner[stage].." (had to make: "..v..") ")
     end
-end
+    sleep(0.01)
 end
 
-if periphemu then
-    handle.writeLine("Check for CCPC configs")
-    if fs.exists("/etc/config/add-debug") then
-        handle.writeLine("Found: /etc/config/add-debug")
-        shell.run("attach bottom debugger")
-        handle.writeLine("attached debugger")
-    end
-end
+print("done")
 
-if periphemu then
-    handle.writeLine("Avoiding bug with keys, suggestion will be passed through with login.")   
-    print("Avoiding bug with keys, suggestion will be passed through with login.")
-    local c = os.clock()
-    handle.writeLine("Reached boot in: "..c.."s")
-    handle.close()
-    if fs.exists("/tmp/ccpcBug") then
-        fs.delete("/tmp/ccpcBug")
-    end
-    fs.copy("/etc/file","/tmp/ccpcBug")
-    sleep(1)
-    shell.run("/boot/dboot.lua")
-else
-    term.setCursorPos(1,10)
-    print("           ENTER for boot, Z for dbios")
-    while true do
-        local event = {os.pullEvent()}
-        local eventD = event[1]
-    
-        if eventD == "key" then
-            local k = event[2]
-            if k == 90 then
-                shell.run("/bin/cls.lua")
-                sleep(0.001)
-                handle.writeLine("Pressed: Z")
-                local c = os.clock()
-                handle.writeLine("Reached boot to dbios in: "..c.."s")
-                handle.close()
-                shell.run("/boot/dbios/init.lua")
-            elseif k == 257 then
-                sleep(0.001)
-                handle.writeLine("Pressed: ENTER")
-                local c = os.clock()
-                handle.writeLine("Reached boot in: "..c.."s")
-                handle.close()
-                shell.run("/boot/dboot.lua")
+x,y = term.getCursorPos()
+for k,v in pairs(bfiles) do
+    if fs.exists(v) then
+        term.setCursorPos(x,y)
+        stage = (stage % 4) + 1
+        write("running basefiles check... ".. spinner[stage].." ")
+    else
+        handle = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dawn/idev/"..v))
+            local toWrite = {}
+            repeat
+                local a = handle.readLine()
+                table.insert(toWrite,a)
+            until a == nil
+            handle.close()
+            handle = fs.open(v,"w")
+            for _,v in pairs(toWrite) do
+                handle.writeLine(v)
             end
-        end
+            handle.close()
+        term.setCursorPos(x,y)
+        stage = (stage % 4) + 1
+        term.clearLine(term.getCursorPos())
+        write("running basefiles check... ".. spinner[stage].." (had to make: "..v..") ")
     end
+    sleep(0.01)
 end
+
+local handle = fs.open("/etc/logs/startup","a")
+handle.writeLine("completed basefs and basefiles checks")
+
+print("done")
+write("resolve env... ")
+if not periphemu then
+    print("CCEmuX/Base CC")
+    handle.writeLine("env is CCEmuX/Base CC")
+else
+    print("CCPC")
+    handle.writeLine("env is CCPC")
+end
+
+handle.writeLine("launching boot program")
+handle.close()
+sleep(1)
+shell.run("/boot/dboot.lua")
